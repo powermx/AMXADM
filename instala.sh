b00f76cd6a4ffdabd5e93264cca7691f5c5e66e8
Decorinmx <63691124+Decorinmx@users.noreply.github.com>
2020-04-15 12:08:30 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -67,7 +67,7 @@ cd /etc/adm-lite
 touch /etc/adm-lite/index.html
 wget -i $HOME/lista -o /dev/null
 echo -e "${cor[3]} $(source trans -b pt:${id} "Instalando Las Dependencias")"
-echo -e "${cor[1]}✯ ➢ ✯ ➣ ✯ ➢ ✯ ➢ ✯ ➣ ✯ ➢ ✯ ➢ ✯ ➣ ✯ ➢ ✯ ➢ ✯ ➣ ✯ ➢ ✯ ➢ ✯ ➣ ✯ ➢ ✯ ➢ ✯ ➢ ✯ ➢ ✯ ➢ ✯ ➢ ✯"
+echo -e "${cor[1]}✯ ➢ ✯ ➣ ✯ ➢ ✯ ➢ ✯ ➣ ✯ ➢ ✯ ➢ ✯ ➣ ✯ ➢ ✯ ➢ ✯"
 cd /etc/adm-lite
 chmod +x ./*
 instalar_fun
